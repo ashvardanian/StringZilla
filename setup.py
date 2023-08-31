50d78caf198224da8f3b963fe190ec1d1e14683d
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-08-31 15:22:22 +0400

    Make: Explicitly UTF-8 encoding on Windows

--- setup.py
+++ setup.py
@@ -64,13 +64,14 @@ __lib_name__ = "stringzilla"
 
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "README.md")) as f:
+with open(os.path.join(this_directory, "README.md"), "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
     name=__lib_name__,
     version=__version__,
+    author="Ash Vardanian",
     description="Crunch 100+ GB Strings in Python with ease",
     long_description=long_description,
     long_description_content_type="text/markdown",
