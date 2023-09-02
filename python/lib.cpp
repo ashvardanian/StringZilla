bc4d2e9053d393643a3f5f88371065abc2ca7a69
r2rSahakyan <89134416+r2rSahakyan@users.noreply.github.com>
2023-09-02 23:35:57 +0400

    Ver First Binding Draft in CPython
    Span class with size , hash, comparison, contains and find functions.
    all added code under #ifndef PURE_CPYTHON

--- python/lib.cpp
+++ python/lib.cpp
@@ -25,6 +25,11 @@ typedef SSIZE_T ssize_t;
 #include <string>      // `std::string`
 #include <string_view> // `std::string_view`
 
+#define PURE_CPYTHON
+#ifdef PURE_CPYTHON
+#include <Python.h>
+#endif
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
@@ -646,6 +651,185 @@ void define_slice_ops(py::class_<at, std::shared_ptr<at>> &str_view_struct) {
         [](at const &s) { return py::make_iterator(s.begin(), s.end()); },
         py::keep_alive<0, 1>());
 }
+
+#ifdef PURE_CPYTHON
+typedef struct
+{
+        PyObject_HEAD
+        std::shared_ptr<py_span_t>  span;
+} PySpan;
+
+
+static PyMethodDef PySpan_methods[] = {
+    {"size", (PyCFunction)PySpan_size, METH_NOARGS, "Get the size"},
+    {"__hash__", (PyCFunction) PySpan_hash, METH_NOARGS, "Returns the hash value"},
+    {"__eq__", (PyCFunction) PySpan_eq, METH_O, "Equality check"},
+    {"__ne__", (PyCFunction) PySpan_ne, METH_O, "Non-equality check"},
+    {"__gt__", (PyCFunction) PySpan_gt, METH_O, "Greater than check"},
+    {"__lt__", (PyCFunction) PySpan_lt, METH_O, "Less than check"},
+    {"contains", (PyCFunction)PyStrView_contains, METH_VARARGS | METH_KEYWORDS, "Check if contains"},
+    {"find", (PyCFunction)PyStrView_find, METH_VARARGS | METH_KEYWORDS, "Find needle"},
+    {NULL}  // Sentinel
+};
+
+static PyObject *PySpan_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+    PySpan *self;
+    self = (PySpan *)type->tp_alloc(type, 0);
+    return (PyObject *)self;
+}
+static int PySpan_init(PySpan *self, PyObject *args, PyObject *kwds)
+{
+    self->span = std::make_shared<py_span_t>();
+    return 0;
+}
+
+static PyObject *PySpan_size(PySpan *self, PyObject *Py_UNUSED(ignored))
+{
+    return PyLong_FromSsize_t(self->span->size());
+}
+
+static void PySpan_dealloc(PySpan *self)
+{
+    // Handle the deallocation of the C++ object
+    self->span.reset();
+    Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyObject* PySpan_hash(PySpan *self) {
+    return PyLong_FromUnsignedLong(self->span->hash());
+}
+
+static PyObject* PySpan_eq(PySpan *self, PyObject *other) {
+    if (PyUnicode_Check(other)) {
+        return PyBool_FromLong(self->span->operator==(PyUnicode_AsUTF8(other)));
+    } else if (PyObject_TypeCheck(other, &PySpanType)) {
+        return PyBool_FromLong(self->span->operator==(((PySpan *)other)->span));
+    }
+    Py_RETURN_FALSE;
+}
+
+static PyObject* PySpan_ne(PySpan *self, PyObject *other) {
+    if (PyUnicode_Check(other)) {
+        return PyBool_FromLong(self->span->operator!=(PyUnicode_AsUTF8(other)));
+    } else if (PyObject_TypeCheck(other, &PySpanType)) {
+        return PyBool_FromLong(self->span->operator!=(((PySpan *)other)->span));
+    }
+    Py_RETURN_TRUE;
+}
+
+static PyObject* PySpan_gt(PySpan *self, PyObject *other) {
+    if (PyUnicode_Check(other)) {
+        return PyBool_FromLong(self->span->operator>(PyUnicode_AsUTF8(other)));
+    } else if (PyObject_TypeCheck(other, &PySpanType)) {
+        return PyBool_FromLong(self->span->operator>(((PySpan *)other)->span));
+    }
+    Py_RETURN_FALSE;
+}
+
+static PyObject* PySpan_lt(PySpan *self, PyObject *other) {
+    if (PyUnicode_Check(other)) {
+        return PyBool_FromLong(self->span->operator<(PyUnicode_AsUTF8(other)));
+    } else if (PyObject_TypeCheck(other, &PySpanType)) {
+        return PyBool_FromLong(self->span->operator<(((PySpan *)other)->span));
+    }
+    Py_RETURN_FALSE;
+}
+
+static PyObject * PyStrView_contains(PyStrView *self, PyObject *args, PyObject *kwargs) {
+    char *needle;
+    int start = 0, end = INT_MAX;
+    static char *kwlist[] = {"needle", "start", "end", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s|ii", kwlist, &needle, &start, &end)) {
+        return NULL;
+    }
+    
+    if(self->span->contains(needle,start,end)){
+        Py_INCREF(Py_True);
+        return Py_True;
+    } else {
+        Py_INCREF(Py_False);
+        return Py_False;
+    }
+}
+static PyObject * PyStrView_find(PyStrView *self, PyObject *args, PyObject *kwargs) {
+    char *needle;
+    int start = 0, end = INT_MAX;
+    static char *kwlist[] = {"needle", "start", "end", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s|ii", kwlist, &needle, &start, &end)) {
+        return NULL;
+    }
+    return PyLong_FromLong(self->span->find(needle,start,end));
+}
+
+static PyTypeObject PySpanType = {
+    PyVarObject_HEAD_INIT(NULL, 0) /* ob_size */
+    "YourModule.Span",             /* tp_name */
+    sizeof(PySpan),                /* tp_basicsize */
+    0,                             /* tp_itemsize */
+    (destructor)PySpan_dealloc,    /* tp_dealloc */
+    0,                             /* tp_print */
+    0,                             /* tp_getattr */
+    0,                             /* tp_setattr */
+    0,                             /* tp_compare */
+    0,                             /* tp_repr */
+    0,                             /* tp_as_number */
+    0,                             /* tp_as_sequence */
+    0,                             /* tp_as_mapping */
+    0,                             /* tp_hash */
+    0,                             /* tp_call */
+    0,                             /* tp_str */
+    0,                             /* tp_getattro */
+    0,                             /* tp_setattro */
+    0,                             /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,            /* tp_flags */
+    "Span objects",                /* tp_doc */
+    0,                             /* tp_traverse */
+    0,                             /* tp_clear */
+    0,                             /* tp_richcompare */
+    0,                             /* tp_weaklistoffset */
+    0,                             /* tp_iter */
+    0,                             /* tp_iternext */
+    PySpan_methods,                /* tp_methods */
+    0,                             /* tp_members */
+    0,                             /* tp_getset */
+    0,                             /* tp_base */
+    0,                             /* tp_dict */
+    0,                             /* tp_descr_get */
+    0,                             /* tp_descr_set */
+    0,                             /* tp_dictoffset */
+    (initproc)PySpan_init,         /* tp_init */
+    0,                             /* tp_alloc */
+    PySpan_new,                    /* tp_new */
+};
+
+static PyModuleDef stringzilla_module = {
+    PyModuleDef_HEAD_INIT,
+    "stringzilla",
+    "Crunch 100+ GB Strings in Python with ease",
+    -1,
+    NULL, NULL, NULL, NULL, NULL
+};
+
+PyMODINIT_FUNC PyInit_stringzilla(void) {
+    PyObject *m;
+
+    if (PyType_Ready(&PySpanType) < 0)
+        return NULL;
+
+    m = PyModule_Create(&stringzilla_module);
+    if (m == NULL)
+        return NULL;
+
+    Py_INCREF(&PySpanType);
+    PyModule_AddObject(m, "Span", (PyObject *)&PySpanType);
+
+    return m;
+}
+
+#endif
 
 PYBIND11_MODULE(stringzilla, m) {
     m.doc() = "Crunch 100+ GB Strings in Python with ease";
