7abc456ca8ea5bf494b9a975f14b909cebba5c53
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-09-18 22:59:56 +0400

    Add: Baseline NodeJS binding
    The current implementation is very weak, as it would cause
    a new copy of the native JS string on every invocation.

---
+++ javascript/stringzilla.d.ts
@@ -0,0 +1,9 @@
+
+/**
+ * Searches for a short string in a long one.
+ * 
+ * @param {string} haystack 
+ * @param {string} needle 
+ */
+export function find(haystack: string, needle: string): bigint;
+  
