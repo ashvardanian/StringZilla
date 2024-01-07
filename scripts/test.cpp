5d729be49421c7c3511aecb8e6bca5fab86c960a
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-01-07 04:49:42 +0000

    Make: GitHub CI for tests with recent GCC

--- scripts/test.cpp
+++ scripts/test.cpp
@@ -6,10 +6,14 @@
 #include <random>    // `std::random_device`
 #include <vector>    // `std::vector`
 
-#define SZ_USE_X86_AVX2 0
-#define SZ_USE_X86_AVX512 1
-#define SZ_USE_ARM_NEON 0
-#define SZ_USE_ARM_SVE 0
+// Overload the following with caution.
+// Those parameters must never be explicitly set during releases,
+// but they come handy during development, if you want to validate
+// different ISA-specific implementations.
+// #define SZ_USE_X86_AVX2 0
+// #define SZ_USE_X86_AVX512 0
+// #define SZ_USE_ARM_NEON 0
+// #define SZ_USE_ARM_SVE 0
 
 #include <string>                      // Baseline
 #include <string_view>                 // Baseline
