93c51ef430a577bfc3c07c65c104194daad6b5af
michaelgrigoryan25 <56165400+michaelgrigoryan25@users.noreply.github.com>
2024-01-11 15:43:45 +0400

    Added more method signatures to the StringZilla String type.

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,3 +1,5 @@
+#![forbid(unused_unsafe, unused_allocation, arithmetic_overflow)]
+
 use std::ffi::CString;
 
 mod bindings {
@@ -7,16 +9,32 @@ mod bindings {
 
 pub type String = bindings::sz_string_view_t;
 
-impl String {
-    pub fn new<'a>(input: impl Into<&'a str>) -> Self {
-        let value = input.into();
+impl<'a> String {
+    pub fn new(input: impl AsRef<str>) -> Self {
+        let value = input.as_ref();
         bindings::sz_string_view_t {
             start: unsafe { CString::new(value).unwrap_unchecked() }.as_ptr() as *const i8,
             length: value.len() as bindings::sz_size_t,
         }
     }
 
-    pub fn count_char<'a>(&self, input: impl Into<&'a str>) -> usize {
+    pub fn find(&self, start: usize, end: usize) -> Option<usize> {
+        unimplemented!()
+    }
+
+    pub fn contains(&self, input: impl Into<&'a str>, start: usize, end: usize) -> bool {
+        unimplemented!()
+    }
+
+    pub fn split_lines(&self, keep_linebreaks: bool, separator: char) -> Vec<String> {
+        unimplemented!()
+    }
+
+    pub fn split(&self, separator: char, max_split: usize, keep_separator: bool) -> Vec<String> {
+        unimplemented!()
+    }
+
+    pub fn count_char(&self, input: impl Into<&'a str>) -> usize {
         let value = input.into();
         unsafe {
             bindings::sz_count_char(
@@ -25,6 +43,21 @@ impl String {
                 CString::new(value).unwrap_unchecked().as_ptr(),
             ) as usize
         }
+    }
+}
+
+impl std::ops::Index<std::ops::Range<usize>> for String {
+    type Output = str;
+
+    #[inline]
+    fn index(&self, index: std::ops::Range<usize>) -> &Self::Output {
+        todo!()
+    }
+}
+
+impl From<std::string::String> for String {
+    fn from(value: std::string::String) -> Self {
+        String::new(value)
     }
 }
 
