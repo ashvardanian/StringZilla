8200ca3894f575d772754c261b769d5852295877
Mikayel Grigoryan <michael.grigoryan25@gmail.com>
2024-01-27 01:24:02 +0400

    Improve: Rust Bindings (#74)
    * Fixed rustdoc
    * Replaced warning flags in the buildscript with a method
    * Enabled no_std compatibility for the library
    * Moved safe operations out of unsafe code blocks
    * Implemented the StringZilla trait

--- build.rs
+++ build.rs
@@ -2,14 +2,9 @@ fn main() {
     cc::Build::new()
         .file("c/lib.c")
         .include("include")
+        .warnings(false)
         .flag_if_supported("-std=c99")
         .flag_if_supported("-fcolor-diagnostics")
-        .flag_if_supported("-Wno-unknown-pragmas")
-        .flag_if_supported("-Wno-unused-function")
-        .flag_if_supported("-Wno-cast-function-type")
-        .flag_if_supported("-Wno-incompatible-function-pointer-types")
-        .flag_if_supported("-Wno-incompatible-pointer-types")
-        .flag_if_supported("-Wno-discarded-qualifiers")
         .flag_if_supported("-fPIC")
         .compile("stringzilla");
 
