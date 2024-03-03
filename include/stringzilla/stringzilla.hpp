bc6688cb4cb9d79d63d3330cf7f99280ae576c09
ashbob999 <ashbob999@gmail.com>
2024-03-03 13:09:58 +0000

    Fixed debug assertions with iterator comparison

--- include/stringzilla/stringzilla.hpp
+++ include/stringzilla/stringzilla.hpp
@@ -458,8 +458,10 @@ class range_matches {
             return temp;
         }
 
-        bool operator!=(iterator const &other) const noexcept { return remaining_.begin() != other.remaining_.begin(); }
-        bool operator==(iterator const &other) const noexcept { return remaining_.begin() == other.remaining_.begin(); }
+        bool operator!=(iterator const &other) const noexcept { return !(*this == other); }
+        bool operator==(iterator const &other) const noexcept {
+            return remaining_.size() == other.remaining_.size() && remaining_.data() == other.remaining_.data();
+        }
         bool operator!=(end_sentinel_type) const noexcept { return !remaining_.empty(); }
         bool operator==(end_sentinel_type) const noexcept { return remaining_.empty(); }
     };
@@ -550,8 +552,10 @@ class range_rmatches {
             return temp;
         }
 
-        bool operator!=(iterator const &other) const noexcept { return remaining_.end() != other.remaining_.end(); }
-        bool operator==(iterator const &other) const noexcept { return remaining_.end() == other.remaining_.end(); }
+        bool operator!=(iterator const &other) const noexcept { return !(*this == other); }
+        bool operator==(iterator const &other) const noexcept {
+            return remaining_.size() == other.remaining_.size() && remaining_.data() == other.remaining_.data();
+        }
         bool operator!=(end_sentinel_type) const noexcept { return !remaining_.empty(); }
         bool operator==(end_sentinel_type) const noexcept { return remaining_.empty(); }
     };
