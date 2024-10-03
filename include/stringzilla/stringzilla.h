696797d1a02de0c1fa0aa56472910317270afefb
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-10-03 16:26:10 +0000

    Improve: Using more registers for small moves
    In AVX-512, similar to GLibC we should use the
    register space to load more data simultaneously
    and avoid loops and data-dependency between
    iterations.

--- include/stringzilla/stringzilla.h
+++ include/stringzilla/stringzilla.h
@@ -4408,7 +4408,7 @@ SZ_PUBLIC void sz_copy_avx512(sz_ptr_t target, sz_cptr_t source, sz_size_t lengt
     // A typical AWS Sapphire Rapids instance can have 48 KB x 2 blocks of L1 data cache per core,
     // 2 MB x 2 blocks of L2 cache per core, and one shared 60 MB buffer of L3 cache.
     // For now, let's avoid the cases beyond the L2 size.
-    int is_huge = length > 2ull * 1024ull * 1024ull;
+    int const is_huge = length > 2ull * 1024ull * 1024ull;
     // When the buffer is small, there isn't much to innovate.
     if (length <= 64) {
         __mmask64 mask = _sz_u64_mask_until(length);
@@ -4476,78 +4476,85 @@ SZ_PUBLIC void sz_copy_avx512(sz_ptr_t target, sz_cptr_t source, sz_size_t lengt
 SZ_PUBLIC void sz_move_avx512(sz_ptr_t target, sz_cptr_t source, sz_size_t length) {
     if (target == source) return; // Don't be silly, don't move the data if it's already there.
 
-    // If the regions don't overlap at all, just use "copy" and save some brain cells thinking about corner cases.
-    if (target + length < source || target >= source + length) {
-        sz_copy_avx512(target, source, length);
-        return;
-    }
-
     // On very short buffers, that are one cache line in width or less, we don't need any loops.
+    // We can also avoid any data-dependencies between iterations, assuming we have 32 registers
+    // to pre-load the data, before writing it back.
     if (length <= 64) {
         __mmask64 mask = _sz_u64_mask_until(length);
         _mm512_mask_storeu_epi8(target, mask, _mm512_maskz_loadu_epi8(mask, source));
-        return;
     }
+    else if (length <= 128) {
+        __mmask64 mask = _sz_u64_mask_until(length % 64);
+        __m512i source0 = _mm512_loadu_epi8(source);
+        __m512i source1 = _mm512_maskz_loadu_epi8(mask, source + 64);
+        _mm512_storeu_epi8(target, source0);
+        _mm512_mask_storeu_epi8(target + 64, mask, source1);
+    }
+    else if (length <= 192) {
+        __mmask64 mask = _sz_u64_mask_until(length % 64);
+        __m512i source0 = _mm512_loadu_epi8(source);
+        __m512i source1 = _mm512_loadu_epi8(source + 64);
+        __m512i source2 = _mm512_maskz_loadu_epi8(mask, source + 128);
+        _mm512_storeu_epi8(target, source0);
+        _mm512_storeu_epi8(target + 64, source1);
+        _mm512_mask_storeu_epi8(target + 128, mask, source2);
+    }
+    else if (length <= 256) {
+        __mmask64 mask = _sz_u64_mask_until(length % 64);
+        __m512i source0 = _mm512_loadu_epi8(source);
+        __m512i source1 = _mm512_loadu_epi8(source + 64);
+        __m512i source2 = _mm512_loadu_epi8(source + 128);
+        __m512i source3 = _mm512_maskz_loadu_epi8(mask, source + 192);
+        _mm512_storeu_epi8(target, source0);
+        _mm512_storeu_epi8(target + 64, source1);
+        _mm512_storeu_epi8(target + 128, source2);
+        _mm512_mask_storeu_epi8(target + 192, mask, source3);
+    }
+
+    // If the regions don't overlap at all, just use "copy" and save some brain cells thinking about corner cases.
+    else if (target + length < source || target >= source + length) { sz_copy_avx512(target, source, length); }
 
     // When the buffer is over 64 bytes, it's guaranteed to touch at least two cache lines - the head and tail,
     // and may include more cache-lines in-between. Knowing this, we can avoid expensive unaligned stores
     // by computing 2 masks - for the head and tail, using masked stores for the head and tail, and unmasked
     // for the body.
-    sz_size_t head_length = (64 - ((sz_size_t)target % 64)) % 64; // 63 or less.
-    sz_size_t tail_length = (sz_size_t)(target + length) % 64;    // 63 or less.
-    sz_size_t body_length = length - head_length - tail_length;   // Multiple of 64.
-    __mmask64 head_mask = _sz_u64_mask_until(head_length);
-    __mmask64 tail_mask = _sz_u64_mask_until(tail_length);
-
-    // The absolute most common case of using "moves" is shifting the data within a continuous buffer
-    // when adding a removing some values in it. In such cases, a typical shift is by 1, 2, 4, 8, 16,
-    // or 32 bytes, rarely larger. For small shifts, under the size of the ZMM register, we can use shuffles.
-    //
-    // Remember:
-    //      - if we are shifting data left, that we are traversing to the right.
-    //      - if we are shifting data right, that we are traversing to the left.
-    int const left_to_right_traversal = source > target;
-
-    // If both targets are equally aligned or misaligned, the efficient implementation is trivial.
-    if ((sz_size_t)target % 64 == (sz_size_t)source % 64) {
-        if (left_to_right_traversal) {
-            // Head, body, and tail.
-            _mm512_mask_storeu_epi8(target, head_mask, _mm512_maskz_loadu_epi8(head_mask, source));
-            for (target += head_length, source += head_length; body_length >= 64;
-                 target += 64, source += 64, body_length -= 64)
-                _mm512_store_si512(target, _mm512_load_si512(source));
-            _mm512_mask_storeu_epi8(target, tail_mask, _mm512_maskz_loadu_epi8(tail_mask, source));
-        }
-        else {
-            // Tail, body, and head.
-            _mm512_mask_storeu_epi8(target + head_length + body_length, tail_mask,
-                                    _mm512_maskz_loadu_epi8(tail_mask, source + head_length + body_length));
-            for (; body_length >= 64; body_length -= 64)
-                _mm512_store_si512(target + head_length + body_length - 64,
-                                   _mm512_load_si512(source + head_length + body_length - 64));
-            _mm512_mask_storeu_epi8(target, head_mask, _mm512_maskz_loadu_epi8(head_mask, source));
-        }
-    }
-
-    // Now we guarantee, that the relative shift within is from 1 to 63 bytes and the output is aligned.
-    // Hopefully, we need to shift more than two ZMM registers, so we could consider `valignr` instruction.
-    // Sadly, using `_mm512_alignr_epi8` doesn't make sense, as it operates at a 128-bit granularity.
-    //
-    //      - `_mm256_alignr_epi8` shifts entire 256-bit register, but we need many of them.
-    //      - `_mm512_alignr_epi32` shifts 512-bit chunks, but only if the `shift` is a multiple of 4 bytes.
-    //      - `_mm512_alignr_epi64` shifts 512-bit chunks by 8 bytes.
-    //
-    // All of those have a latency of 1 cycle, and the shift amount must be an immediate value!
-    // For 1-byte-shift granularity, the `_mm512_permutex2var_epi8` has a latency of 6 and needs VBMI!
-    // The most efficient and broadly compatible alternative could be to use a combination of align and shuffle.
-    // A similar approach was outlined in "Byte-wise alignr in AVX512F" by Wojciech Muła.
-    // http://0x80.pl/notesen/2016-10-16-avx512-byte-alignr.html
-    //
-    // That solution, is extremely mouthful, assuming we need compile time constants for the shift amount.
-    // A cleaner one, with a latency of 3 cycles, is to use `_mm512_permutexvar_epi8` or `_mm512_mask_permutexvar_epi8`,
-    // which can be seen as combination of a cross-register shuffle and blend, and is available with VBMI.
-    // That solution is still noticeably slower than AVX2.
     else {
+        sz_size_t head_length = (64 - ((sz_size_t)target % 64)) % 64; // 63 or less.
+        sz_size_t tail_length = (sz_size_t)(target + length) % 64;    // 63 or less.
+        sz_size_t body_length = length - head_length - tail_length;   // Multiple of 64.
+        __mmask64 head_mask = _sz_u64_mask_until(head_length);
+        __mmask64 tail_mask = _sz_u64_mask_until(tail_length);
+
+        // The absolute most common case of using "moves" is shifting the data within a continuous buffer
+        // when adding a removing some values in it. In such cases, a typical shift is by 1, 2, 4, 8, 16,
+        // or 32 bytes, rarely larger. For small shifts, under the size of the ZMM register, we can use shuffles.
+        //
+        // Remember:
+        //      - if we are shifting data left, that we are traversing to the right.
+        //      - if we are shifting data right, that we are traversing to the left.
+        int const left_to_right_traversal = source > target;
+
+        // Now we guarantee, that the relative shift within registers is from 1 to 63 bytes and the output is aligned.
+        // Hopefully, we need to shift more than two ZMM registers, so we could consider `valignr` instruction.
+        // Sadly, using `_mm512_alignr_epi8` doesn't make sense, as it operates at a 128-bit granularity.
+        //
+        //      - `_mm256_alignr_epi8` shifts entire 256-bit register, but we need many of them.
+        //      - `_mm512_alignr_epi32` shifts 512-bit chunks, but only if the `shift` is a multiple of 4 bytes.
+        //      - `_mm512_alignr_epi64` shifts 512-bit chunks by 8 bytes.
+        //
+        // All of those have a latency of 1 cycle, and the shift amount must be an immediate value!
+        // For 1-byte-shift granularity, the `_mm512_permutex2var_epi8` has a latency of 6 and needs VBMI!
+        // The most efficient and broadly compatible alternative could be to use a combination of align and shuffle.
+        // A similar approach was outlined in "Byte-wise alignr in AVX512F" by Wojciech Muła.
+        // http://0x80.pl/notesen/2016-10-16-avx512-byte-alignr.html
+        //
+        // That solution, is extremely mouthful, assuming we need compile time constants for the shift amount.
+        // A cleaner one, with a latency of 3 cycles, is to use `_mm512_permutexvar_epi8` or
+        // `_mm512_mask_permutexvar_epi8`, which can be seen as combination of a cross-register shuffle and blend,
+        // and is available with VBMI. That solution is still noticeably slower than AVX2.
+        //
+        // The GLibC implementation also uses non-temporal stores for larger buffers, we don't.
+        // https://codebrowser.dev/glibc/glibc/sysdeps/x86_64/multiarch/memmove-avx512-no-vzeroupper.S.html
         if (left_to_right_traversal) {
             // Head, body, and tail.
             _mm512_mask_storeu_epi8(target, head_mask, _mm512_maskz_loadu_epi8(head_mask, source));
