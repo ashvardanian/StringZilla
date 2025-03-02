6f161a4d706b4400351c8bcdf94871cfe8672594
TinySemVer <tinysemver@ashvardanian.com>
2025-03-02 11:08:10 +0000

    Release: v3.12.2 [skip ci] ### Patch
    - Fix: Horspool corner-case (#216) (ffa4518)

--- include/stringzilla/stringzilla.h
+++ include/stringzilla/stringzilla.h
@@ -25,7 +25,7 @@
 
 #define STRINGZILLA_VERSION_MAJOR 3
 #define STRINGZILLA_VERSION_MINOR 12
-#define STRINGZILLA_VERSION_PATCH 1
+#define STRINGZILLA_VERSION_PATCH 2
 
 /**
  *  @brief  When set to 1, the library will include the following LibC headers: <stddef.h> and <stdint.h>.
