197535191ebc44a5776e7494ed88ae7b2661e8c6
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2025-04-26 21:37:18 +0000

    Add: Draft StringCuZilla C API

--- include/stringcuzilla/stringcuzilla.h
+++ include/stringcuzilla/stringcuzilla.h
@@ -16,19 +16,76 @@
 extern "C" {
 #endif
 
-SZ_DYNAMIC sz_status_t sz_levenshtein_distances_u32tape( //
-    sz_cptr_t a_data, sz_u32_t const *a_lengths,         //
-    sz_cptr_t b_data, sz_u32_t const *b_lengths,         //
-    sz_size_t count,                                     //
-    sz_size_t bound,                                     //
-    sz_memory_allocator_t *alloc, sz_size_t *results);
+struct sz_arrow_u32tape_t {
+    sz_cptr_t data;
+    sz_u32_t const *lengths;
+    sz_size_t count;
+};
 
-SZ_DYNAMIC sz_status_t sz_needleman_wunsch_scores_u32tape( //
-    sz_cptr_t a_data, sz_u32_t const *a_lengths,           //
-    sz_cptr_t b_data, sz_u32_t const *b_lengths,           //
-    sz_size_t count,                                       //
-    sz_error_cost_t const *subs, sz_error_cost_t gap,      //
-    sz_memory_allocator_t *alloc, sz_ssize_t *results);
+struct sz_arrow_u64tape_t {
+    sz_cptr_t data;
+    sz_u64_t const *lengths;
+    sz_size_t count;
+};
+
+/**
+ *  Doesn't aim to provide the same level of granularity as the C++ API.
+ *  It expects that the C functions will be called in bulk, generally,
+ *  by just a single caller, either targeting:
+ *
+ *  - a single CPU core,
+ *  - a fraction of CPU cores through some global thread pool,
+ *  - a single GPU device.
+ */
+struct sz_device_scope_t {
+    sz_ssize_t cpu_cores;
+    sz_ssize_t gpu_device;
+};
+
+SZ_DYNAMIC sz_status_t sz_levenshtein_distances_sequence(                                          //
+    sz_sequence_t const *a, sz_sequence_t const *b,                                                //
+    sz_error_cost_t match, sz_error_cost_t mismatch, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_size_t *results);
+
+SZ_DYNAMIC sz_status_t sz_needleman_wunsch_scores_sequence(                    //
+    sz_sequence_t const *a, sz_sequence_t const *b,                            //
+    sz_error_cost_t const *subs, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_ssize_t *results);
+
+SZ_DYNAMIC sz_status_t sz_smith_waterman_scores_sequence(                      //
+    sz_sequence_t const *a, sz_sequence_t const *b,                            //
+    sz_error_cost_t const *subs, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_ssize_t *results);
+
+SZ_DYNAMIC sz_status_t sz_levenshtein_distances_u32tape(                                           //
+    sz_arrow_u32tape_t const *a, sz_arrow_u32tape_t const *b,                                      //
+    sz_error_cost_t match, sz_error_cost_t mismatch, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_size_t *results);
+
+SZ_DYNAMIC sz_status_t sz_needleman_wunsch_scores_u32tape(                     //
+    sz_arrow_u32tape_t const *a, sz_arrow_u32tape_t const *b,                  //
+    sz_error_cost_t const *subs, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_ssize_t *results);
+
+SZ_DYNAMIC sz_status_t sz_smith_waterman_scores_u32tape(                       //
+    sz_arrow_u32tape_t const *a, sz_arrow_u32tape_t const *b,                  //
+    sz_error_cost_t const *subs, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_ssize_t *results);
+
+SZ_DYNAMIC sz_status_t sz_levenshtein_distances_u64tape(                                           //
+    sz_arrow_u64tape_t const *a, sz_arrow_u64tape_t const *b,                                      //
+    sz_error_cost_t match, sz_error_cost_t mismatch, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_size_t *results);
+
+SZ_DYNAMIC sz_status_t sz_needleman_wunsch_scores_u64tape(                     //
+    sz_arrow_u64tape_t const *a, sz_arrow_u64tape_t const *b,                  //
+    sz_error_cost_t const *subs, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_ssize_t *results);
+
+SZ_DYNAMIC sz_status_t sz_smith_waterman_scores_u64tape(                       //
+    sz_arrow_u64tape_t const *a, sz_arrow_u64tape_t const *b,                  //
+    sz_error_cost_t const *subs, sz_error_cost_t open, sz_error_cost_t extend, //
+    sz_memory_allocator_t *alloc, sz_device_scope_t const *device, sz_ssize_t *results);
 
 #ifdef __cplusplus
 }
