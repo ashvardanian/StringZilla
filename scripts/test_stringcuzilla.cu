7a433a915b64441b7a2b3b876cf6cbbd8b1699b4
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2025-07-09 13:48:52 +0000

    Improve: Prioritize find-many tests

--- scripts/test_stringcuzilla.cu
+++ scripts/test_stringcuzilla.cu
@@ -38,9 +38,9 @@ int main(int argc, char const **argv) {
     if (auto code = sz::scripts::log_environment(); code != 0) return code;
 
     try {
+        sz::scripts::test_find_many_equivalence();
         sz::scripts::test_similarity_scores_equivalence();
         sz::scripts::test_similarity_scores_memory_usage();
-        sz::scripts::test_find_many_equivalence();
     }
     catch (std::exception const &e) {
         std::fprintf(stderr, "Failed with: %s\n", e.what());
